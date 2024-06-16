2669394fcb455fc734df0910d5434393df2be32d
valentinefleith <valentine.fleith@gmail.com>
2024-06-16 12:14:23 +0200

    little fixes

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 12:08:04 by vafleith          #+#    #+#             */
-/*   Updated: 2024/06/16 12:10:26 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/06/16 12:13:32 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <pthread.h>
+# include <sys/time.h>
 
 #endif
