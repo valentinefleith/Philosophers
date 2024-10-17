2e43adee49d6887b551a780fbb68d5f20eed42a9
valentinefleith <valentine.fleith@gmail.com>
2024-10-17 16:36:16 +0200

    print sleep + colors

--- src/print/error_messages.c
+++ src/print/error_messages.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 22:16:19 by vafleith          #+#    #+#             */
-/*   Updated: 2024/06/25 22:47:44 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/10/17 16:19:43 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	argument_error(void)
 {
-	ft_putstr_fd("Usage: ./philo nb_of_philo time_to_die time_to_eat", 2);
+	ft_putstr_fd("Usage: ./philo nb_of_philo time_to_die time_to_eat ", 2);
 	ft_putendl_fd("time_to_sleep [nb_of_times_each_philo_must_eat]", 2);
 	exit(1);
 }
--- src/simulate/simulation.c
+++ src/simulate/simulation.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 23:30:28 by vafleith          #+#    #+#             */
-/*   Updated: 2024/10/17 16:18:21 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/10/17 16:36:10 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,16 @@ static void	print_philologs(char *log, t_philosopher *philo)
 
 	timestamp = get_current_time_ms() - philo->dinner_table->start_time;
 	pthread_mutex_lock(&philo->dinner_table->print_guardian);
+	if (philo->id == 0)
+		printf("\033[0;32m");
+	else if (philo->id == 1)
+		printf("\033[0;33m");
+	else if (philo->id == 2)
+		printf("\033[0;34m");
+	else if (philo->id == 3)
+		printf("\033[0;35m");
 	printf("at %li ms \tphilo nb %i\t%s\n", timestamp, philo->id, log);
+	printf("\033[0m");
 	pthread_mutex_unlock(&philo->dinner_table->print_guardian);
 }
 
@@ -41,12 +50,22 @@ static void	philo_miam(t_philosopher *philo)
 	pthread_mutex_unlock(&table->forks[philo->second_fork_id]);
 }
 
+static void	philo_zzz(t_philosopher *philo)
+{
+	print_philologs("is sleeping", philo);
+	sleep_boosted(philo->dinner_table->rules.time_to_sleep);
+}
+
 static void	*routine(void *params)
 {
 	t_philosopher	*philo;
 
 	philo = (t_philosopher *)params;
-	philo_miam(philo);
+	while (1)
+	{
+		philo_miam(philo);
+		philo_zzz(philo);
+	}
 	return (NULL);
 }
 
